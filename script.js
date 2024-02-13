19cae0e824cc2881de362a9a68e6edbc605527e4
Beau Oster <osterbeau@gmail.com>
2024-02-13 13:32:00 -0600

    This is the first commit to practice (DEMO)

---
+++ script.js
@@ -0,0 +1,7 @@
+//var clicks = 0;
+// This is a comment
+//const clickElement = documnet.getElementByID("myButton");
+
+//clickElement.addEventListener('click', function() {
+//    clicks += 1;
+// });
