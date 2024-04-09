c80035270ae023c5efd00dee19fc87a0fc5e4a87
Lynn Gobina <lynnashley.gobina@my.uwrf.edu>
2024-04-09 13:08:29 -0500

    Added navbar and footer - Lynn (4/9/2024)

---
+++ hospital-manager/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer(){
+    return(
+        <footer>
+            <ul>
+                <li><a href="#contact">Contact Customer Service</a></li>
+                <li><a href="#email">Reach Us By Email</a></li> 
+            </ul>
+        </footer>
+    );
+}
---
+++ hospital-manager/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function NavBar(){
+    return(
+        <div>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#signup">Sign Up</a></li>
+                <li><a href="#log in">Log In</a></li>
+                <li><a href="#viewrecords">View Records</a></li> 
+                <li><a href="#inputrecords">Input New Records</a></li>
+            </ul>
+        </div>
+    );
+}
